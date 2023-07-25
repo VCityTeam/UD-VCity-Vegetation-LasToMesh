57d5dcd6f77cee0da5c23619b84804058ca99130
JulienPercheron <julien_p@icloud.com>
2023-07-25 14:31:59 +0200

    Removed need of ply file and convert

--- meshCreationUtilities.py
+++ meshCreationUtilities.py
@@ -5,7 +5,6 @@ def pointToColor(point):
     return str(point[0])+str(point[1])+str(point[2])
 
 def clearFolders(path):
-    path_output = path+"obj/"
 
     try:
         onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
@@ -14,12 +13,22 @@ def clearFolders(path):
     except FileNotFoundError:
         mkdir(path)
     
-    try:
-        onlyfiles = [f for f in listdir(path_output) if isfile(join(path_output, f))]
-        for file in onlyfiles:
-            remove(path_output+str(file))
-    except FileNotFoundError:
-        # No obj folder existing
-        mkdir(path_output)
-        return
+
     
+def exportHullInOBJ(vertices, colors, triangles, outputpath):
+    
+    obj = "# hull\n"
+    
+    # Vertex coordinates + color
+    for i in range(len(vertices)):
+        obj += "v " + "{:.8f}".format(vertices[i][0]) + " " + "{:.8f}".format(vertices[i][1]) + " " + "{:.8f}".format(vertices[i][2]) + " "
+        obj += "{:.8f}".format(colors[i][0]) + " " + "{:.8f}".format(colors[i][0]) + " " + "{:.8f}".format(colors[i][0]) + "\n"
+
+    # Triangles
+    for face in triangles:
+        obj += "f " + str(face[0]+1) + " " + str(face[1]+1) + " " + str(face[2]+1) + "\n"
+
+    # Output
+    with open(outputpath, 'w') as f:
+        for line in obj:
+            f.write(line)
