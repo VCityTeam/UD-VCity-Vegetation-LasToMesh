49e362da7495968a1e0b92df90e1bf6a2c976a9d
JulienPercheron <julien_p@icloud.com>
2023-07-26 17:01:39 +0200

    Added comment on the obj export

--- meshCreationUtilities.py
+++ meshCreationUtilities.py
@@ -17,15 +17,17 @@ def clearFolders(path):
     
 def exportHullInOBJ(vertices, colors, triangles, outputpath):
     
-    obj = "# hull\n"
+    obj = ""
     
     # Vertex coordinates + color
     for i in range(len(vertices)):
+        # Formating to always have 8 digits in the decimals
         obj += "v " + "{:.8f}".format(vertices[i][0]) + " " + "{:.8f}".format(vertices[i][1]) + " " + "{:.8f}".format(vertices[i][2]) + " "
         obj += "{:.8f}".format(colors[i][0]) + " " + "{:.8f}".format(colors[i][0]) + " " + "{:.8f}".format(colors[i][0]) + "\n"
 
     # Triangles
     for face in triangles:
+        # Careful : the index start at 0 in the input but must start at 1 in the output
         obj += "f " + str(face[0]+1) + " " + str(face[1]+1) + " " + str(face[2]+1) + "\n"
 
     # Output
