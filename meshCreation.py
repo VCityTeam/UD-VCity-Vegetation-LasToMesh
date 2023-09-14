300e8520d592be104a6e71af805dd94745294990
JulienPercheron <julien_p@icloud.com>
2023-09-14 14:18:05 +0200

    Added comments

--- meshCreation.py
+++ meshCreation.py
@@ -106,8 +106,10 @@ def main():
     start = time.time()
     print("Starting process of vertices")
 
+    # This variable is used for the implementation of canopy reconstruction. It is no longer used so it has been commented out
+    #
     # Creation of the obj file through a string
-    objfile = "# Vegetation.obj \n# \n\n"
+    # objfile = "# Vegetation.obj \n# \n\n"
 
     cellsVertexIndex = np.zeros((cellCountWidth, cellCountHeight), np.int32)
 
@@ -119,13 +121,11 @@ def main():
             if(cellsZmean[i][j] > 0.0):
                 xcoord = i * cellSize + xmin 
                 ycoord = j * cellSize + ymin
-                # The color via vertex color is still not implemented in py3dtilers so it's commented out for now
-                # objfile += "v " + str(xcoord) + " " + str(ycoord) + " " +  str(cellsZmean[i][j]) + " 0.0 0.6 0.0\n"
-                objfile += "v " + str(xcoord) + " " + str(ycoord) + " " +  str(cellsZmean[i][j]) + "\n"
+                # objfile += "v " + str(xcoord) + " " + str(ycoord) + " " +  str(cellsZmean[i][j]) + "\n"
                 cellsVertexIndex[i][j] = index
                 index += 1
 
-    objfile += "\n"
+    # objfile += "\n"
 
     end = time.time()
     print("Finished process of vertices in " + str(end - start) + " seconds") # time in seconds
@@ -137,6 +137,8 @@ def main():
     cellsIsletIndex = np.zeros((cellCountWidth, cellCountHeight), np.int32)
     index_islet = 1
 
+    # Currently, this part is only used to calculate the islet ids the parts used to calculate the canopy have been commented out
+    #
     # Checking the current cell and the other three on a given corner (here curentx +1 and current y+1)
     # If at least 3 of them have a height, draw a triangle using those cells' center as vertices
     # If the four of them have a height, draw two triangles instead
@@ -147,31 +149,19 @@ def main():
                 
                 # First triangle
 
-                # Vertex coloring version
-                
-                # objfile += "f "
-                # objfile += str(cellsVertexIndex[i][j]) + "//" + str(cellsVertexIndex[i][j]) + " "
-                # objfile += str(cellsVertexIndex[i+1][j]) + "//" + str(cellsVertexIndex[i+1][j]) + " "
-                # objfile += str(cellsVertexIndex[i][j+1]) + "//" + str(cellsVertexIndex[i][j+1]) + "\n"
 
-                objfile += "f "
-                objfile += str(cellsVertexIndex[i][j]) + " "
-                objfile += str(cellsVertexIndex[i+1][j]) + " "
-                objfile += str(cellsVertexIndex[i][j+1]) + "\n"
+                # objfile += "f "
+                # objfile += str(cellsVertexIndex[i][j]) + " "
+                # objfile += str(cellsVertexIndex[i+1][j]) + " "
+                # objfile += str(cellsVertexIndex[i][j+1]) + "\n"
 
                 # Second triangle
 
-                # Vertex coloring version
 
                 # objfile += "f "
-                # objfile += str(cellsVertexIndex[i][j+1]) + "//" + str(cellsVertexIndex[i][j+1]) + " "
-                # objfile += str(cellsVertexIndex[i+1][j]) + "//" + str(cellsVertexIndex[i+1][j]) + " "
-                # objfile += str(cellsVertexIndex[i+1][j+1]) + "//" + str(cellsVertexIndex[i+1][j+1]) + "\n"
-
-                objfile += "f "
-                objfile += str(cellsVertexIndex[i][j+1]) + " "
-                objfile += str(cellsVertexIndex[i+1][j]) + " "
-                objfile += str(cellsVertexIndex[i+1][j+1]) + "\n"
+                # objfile += str(cellsVertexIndex[i][j+1]) + " "
+                # objfile += str(cellsVertexIndex[i+1][j]) + " "
+                # objfile += str(cellsVertexIndex[i+1][j+1]) + "\n"
 
                 # Indexing the islet number
 
@@ -257,18 +247,11 @@ def main():
             elif ((cellsZmean[i][j]) > 0 and (cellsZmean[i+1][j]) > 0 and (cellsZmean[i+1][j+1]) > 0 and (cellsZmean[i][j+1]) == 0):
                 # Main angle in the bottom right corner
                 
-                # Vertex coloring version
 
                 # objfile += "f "
-                # objfile += str(cellsVertexIndex[i][j]) + "//" + str(cellsVertexIndex[i][j]) + " "
-                # objfile += str(cellsVertexIndex[i+1][j]) + "//" + str(cellsVertexIndex[i+1][j]) + " "
-                # objfile += str(cellsVertexIndex[i+1][j+1]) + "//" + str(cellsVertexIndex[i+1][j+1]) + "\n"
-
-
-                objfile += "f "
-                objfile += str(cellsVertexIndex[i][j]) + " "
-                objfile += str(cellsVertexIndex[i+1][j]) + " "
-                objfile += str(cellsVertexIndex[i+1][j+1]) + "\n"
+                # objfile += str(cellsVertexIndex[i][j]) + " "
+                # objfile += str(cellsVertexIndex[i+1][j]) + " "
+                # objfile += str(cellsVertexIndex[i+1][j+1]) + "\n"
 
                 # Indexing the islet number
 
@@ -325,17 +308,10 @@ def main():
             elif ((cellsZmean[i][j]) > 0 and (cellsZmean[i+1][j]) > 0 and (cellsZmean[i+1][j+1]) == 0 and (cellsZmean[i][j+1]) > 0):
                 # Main angle in the bottom left corner
                 
-                # Vertex coloring version
-
                 # objfile += "f "
-                # objfile += str(cellsVertexIndex[i][j]) + "//" + str(cellsVertexIndex[i][j]) + " "
-                # objfile += str(cellsVertexIndex[i+1][j]) + "//" + str(cellsVertexIndex[i+1][j]) + " "
-                # objfile += str(cellsVertexIndex[i][j+1]) + "//" + str(cellsVertexIndex[i][j+1]) + "\n" 
-                
-                objfile += "f "
-                objfile += str(cellsVertexIndex[i][j]) + " "
-                objfile += str(cellsVertexIndex[i+1][j]) + " "
-                objfile += str(cellsVertexIndex[i][j+1]) + "\n" 
+                # objfile += str(cellsVertexIndex[i][j]) + " "
+                # objfile += str(cellsVertexIndex[i+1][j]) + " "
+                # objfile += str(cellsVertexIndex[i][j+1]) + "\n" 
 
                 # Indexing the islet number
 
@@ -394,18 +370,12 @@ def main():
 
             elif ((cellsZmean[i][j]) > 0 and (cellsZmean[i+1][j]) == 0 and (cellsZmean[i+1][j+1]) > 0 and (cellsZmean[i][j+1]) > 0):
                 # Main angle in the top left corner
-                
-                # Vertex coloring version
+
 
                 # objfile += "f "
-                # objfile += str(cellsVertexIndex[i][j]) + "//" + str(cellsVertexIndex[i][j]) + " "
-                # objfile += str(cellsVertexIndex[i+1][j+1]) + "//" + str(cellsVertexIndex[i+1][j+1]) + " "
-                # objfile += str(cellsVertexIndex[i][j+1]) + "//" + str(cellsVertexIndex[i][j+1]) + "\n"
-
-                objfile += "f "
-                objfile += str(cellsVertexIndex[i][j]) + " "
-                objfile += str(cellsVertexIndex[i+1][j+1]) + " "
-                objfile += str(cellsVertexIndex[i][j+1]) + "\n"
+                # objfile += str(cellsVertexIndex[i][j]) + " "
+                # objfile += str(cellsVertexIndex[i+1][j+1]) + " "
+                # objfile += str(cellsVertexIndex[i][j+1]) + "\n"
 
                 # Indexing the islet number
 
@@ -459,17 +429,11 @@ def main():
             elif ((cellsZmean[i][j]) == 0 and (cellsZmean[i+1][j]) > 0 and (cellsZmean[i+1][j+1]) > 0 and (cellsZmean[i][j+1]) > 0):
                 # Main angle in the top right corner
                 
-                # Vertex coloring version
 
                 # objfile += "f "
-                # objfile += str(cellsVertexIndex[i][j+1]) + "//" + str(cellsVertexIndex[i][j+1]) + " "
-                # objfile += str(cellsVertexIndex[i+1][j]) + "//" + str(cellsVertexIndex[i+1][j]) + " "
-                # objfile += str(cellsVertexIndex[i+1][j+1]) + "//" + str(cellsVertexIndex[i+1][j+1]) + "\n"
-
-                objfile += "f "
-                objfile += str(cellsVertexIndex[i][j+1]) + " "
-                objfile += str(cellsVertexIndex[i+1][j]) + " "
-                objfile += str(cellsVertexIndex[i+1][j+1]) + "\n"
+                # objfile += str(cellsVertexIndex[i][j+1]) + " "
+                # objfile += str(cellsVertexIndex[i+1][j]) + " "
+                # objfile += str(cellsVertexIndex[i+1][j+1]) + "\n"
 
                 # Indexing the islet number
 
@@ -530,9 +494,20 @@ def main():
     start = time.time()
     print("Starting separating islets")
 
+    # Separate islands of points based on the island id calculated above
+    # and put the informations in different structures
+
+    # Dict containing in keys the id of the island and in values the list of points of the island (x, y, z) 
     islands = {}
+    # Used a bit bellow, contains in keys the island's id and in value its size (based on the cellsize)
     islands_size = {}
+    # Dict containing the color associated with the point
+    # The key is obtained by passing the coordinates to the 'pointToColor' function
+    # The value is the RGB value of the point, ranging from 0 to 255
     islands_color = {}
+    # Dict containing the normalized color associated with the point
+    # The key is obtained by passing the coordinates to the 'pointToColor' function
+    # The value is the RGB value of the point, ranging from 0 to 1
     islands_color_normalized = {}
 
     for i in range(indexes.shape[0]):
@@ -546,7 +521,6 @@ def main():
     
             islands[cellsIsletIndex[xi][yi]].append(point_data[i])
 
-            #islands_color[cellsIsletIndex[xi][yi]].append([0.01,0.9,0.01])
             islands_color[MeshUtilities.pointToColor(point_data[i])] = point_data_color[i]
             islands_color_normalized[MeshUtilities.pointToColor(point_data[i])] = point_data_color_normalized[i]
             
@@ -558,13 +532,13 @@ def main():
     startoutput = time.time()
     print("Starting output of islets convex hull/alpha shapes")
 
-    geom = o3d.geometry.PointCloud()
-
+    # For debuging purposes
     choice = ""
 
     for index in islands:
 
         condition = cellsIsletIndex == index
+        # Count the number of cells the island is spanning 
         islands_size[index] = np.count_nonzero(condition)
         
         # If the island is too small, don't create a mesh for it
@@ -581,26 +555,34 @@ def main():
                     choice += str(islands_size[index]) + " convex hull\n"
 
                 except Exception as exce:
-                    # Sometimes, error happens due to the shape of the point cloud (needs to be confirmed)
+                    # Sometimes, error happens due to the shape of the point cloud
+                    # we use the alpha shape algorithm instead
                     print("[Warning] : error when creating the convex hull, aborting")
                     print(exce)
                     choice += str(islands_size[index]) + " convex hull ERROR\n"
+            # If the island is too big, create an extruded 2D alpha shape
             elif(islands_size[index] > 10000):
+                # Alpha parameter
                 alpha = MeshUtilities.computeAlpha(islands[index])
                 
                 path = output_path+"alpha_extruded_"+ str(index) + ".obj"
                         
                 MeshUtilities.createExtruded2DAlphaShape(islands[index], islands_color_normalized, alpha, path)
+            # If it's not too big or too small, try doing a layered alpha shape
             else:
                 try:
 
                     # Divide the island in layers based on the z axis
                     foo, foo, zmax = np.max(islands[index], axis=0)
                     foo, foo, zmin = np.min(islands[index], axis=0)
+                    
+                    # number of slices we want from our island
                     nbLayers = 5
+                    # We take a portion of the upper slice to smooth the transition between the meshes
                     margin = ((zmax-zmin)/nbLayers)/3
 
                     for layer in range(1,(nbLayers+1)):
+                        # Compute the bounding heights 
                         thresholdmax = zmin + (((zmax-zmin)/nbLayers)*layer)
                         thresholdmin = zmin + (((zmax-zmin)/(nbLayers))*(layer-1))
                         layerPoints = []
@@ -624,6 +606,7 @@ def main():
                     print("[Warning] : error when creating the layered alphashape, aborting")
                     print(exce)
                     try:
+                        # Simple alpha shape
 
                         alpha = MeshUtilities.computeAlpha(islands[index])
                         path = output_path+"alpha_"+ str(index) + ".obj"
@@ -635,6 +618,7 @@ def main():
                         try:
                             print("[Warning] : error when creating the alphashape, aborting")
                             print(exce)
+                            # If even the simple alpha shape failed, try creating a convex hull
 
                             path = output_path+"hull_"+ str(index) +".obj"
 
@@ -654,23 +638,24 @@ def main():
 
     start = time.time()
     print("Starting writing of output file")
-
-    # Summary of the size of the island, the choice made by the algorithm and if there was an error
+    
+    # For debug
+    #
+    # Summary of the size of the island, the script's algorithm choice and if there was an error
+    """
     with open('choice.txt', 'w') as f:
         for line in choice:
             f.write(line)
-
+    """
+            
+    # Export for the canopy algorithm
+    #
     # Write all the lines in the obj file
+    """
     with open('output.obj', 'w') as f:
         for line in objfile:
             f.write(line)
-
-    # text form of the islands cut
-    with open('outputIslet.txt', 'w') as f:
-        for i in range(cellsIsletIndex.shape[0]):
-            f.write("\n")
-            for j in range(cellsIsletIndex.shape[1]):
-                f.write(str(cellsIsletIndex[i][j]) + " ")
+    """
 
     end = time.time()
     print("Finished writing of output file in " + str(end - start) + " seconds") # time in seconds
--- meshCreationUtilities.py
+++ meshCreationUtilities.py
@@ -12,10 +12,28 @@ import triangulate
 
 
 def pointToColor(point):
+    """ Returns a string that can be used as a key based on a point's coordinates (for example the color dict)
+    
+    Parameters
+    ------
+    point : array
+      Array that contains the x, y and z coordinates of the point
+
+    Returns
+    -------
+    index : str
+        String that can be used as a key
+    """
     return str(point[0])+str(point[1])+str(point[2])
 
 def clearFolders(path):
-
+    """ Clear all of the existing files inside a folder
+    
+    Parameters
+    ------
+    path : str
+      Path to the folder
+    """
     try:
         onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
         for file in onlyfiles:
@@ -26,7 +44,24 @@ def clearFolders(path):
 
     
 def exportHullInOBJ(vertices, colors, triangles, outputpath):
+    """ Create an obj file using vertices, colors and triangles provided
     
+    Parameters
+    ------
+    vertices : 2d-array
+      In column, index of the point.  
+      In line, array that contains the x, y and z coordinates of the point.
+    colors : 2d-array
+      In column, index of the point.  
+      In line, array that contains the r, g and b values of the point.
+    triangles : 2d-array
+      In column, index of the triangle.  
+      In line, array that contains the indexes of points making the triangle.
+    path : str
+        Path to save the obj file
+
+    """
+
     obj = ""
     
     # Vertex coordinates + color
@@ -46,6 +81,20 @@ def exportHullInOBJ(vertices, colors, triangles, outputpath):
             f.write(line)
 
 def createConvexHull(pointCloud, colors_normalized, path):
+    """ Create a convex hull mesh based on a provided point cloud and save it as an obj file
+    
+    Parameters
+    ------
+    pointCloud : 2d-array
+        In column, index of the point.  
+        In line, array that contains the x, y and z coordinates of the point.
+    colors_normalized : 2d-array
+        In column, index of the point.  
+        In line, array that contains the r, g and b values of the point (between 0 and 1).
+    path : str
+        Path to save the obj file
+
+    """
     # Creating the hull
     geom = o3d.geometry.PointCloud()
 
@@ -64,9 +113,22 @@ def createConvexHull(pointCloud, colors_normalized, path):
 
 
 def computeAlpha(pointCloud):
-    # Parameter between 1 and 0 dictating the level of detail of the alpha shape.
-    # The closer you are to 1 the more details you'll have, risking having holes in the mesh.
+    """ Compute an alpha parameter for the given point cloud using its size
+    
+    Parameters
+    ------
+    pointCloud : 2d-array
+        In column, index of the point.  
+        In line, array that contains the x, y and z coordinates of the point.
+        
+    Returns
+    -------
+    alpha : float
+        Value to provide to the alpha shape algorithm
+    """
+    # Parameter dictating the level of detail of the alpha shape.
     # The closer you are to 0, the closer you'll be from the convex hull.
+    # The closer you are to Inf the more details you'll have, risking having holes in the mesh.
     # The formula bellow reduce the alpha the more points you have in the island.
     alpha = 1-(math.log(len(pointCloud), 10)*1.8)/10
     if(alpha < 0.01):
@@ -77,9 +139,26 @@ def computeAlpha(pointCloud):
     return alpha
 
 def createExtruded2DAlphaShape(pointCloud, colors, alpha, path):
+    """Create an extruded 2D alpha shape mesh based on a provided point cloud and alpha parameter and save it as an obj file 
+    
+    Parameters
+    ------
+    pointCloud : 2d-array
+        In column, index of the point.  
+        In line, array that contains the x, y and z coordinates of the point.
+    colors : 2d-array
+        In column, index of the point.  
+        In line, array that contains the r, g and b values of the point (between 0 and 255).
+    alpha : float
+        Value dictating the level of detail of the result of the alpha shape algorithm 
+    path : str
+        Path to save the obj file
+
+    """
     minZ = np.PINF
     maxZ = np.NINF
 
+    # Get the min an max of z coordinates
     for point in pointCloud:
         if point[2] < minZ:
             minZ = point[2]
@@ -103,14 +182,18 @@ def createExtruded2DAlphaShape(pointCloud, colors, alpha, path):
     vertices = {}
     obj = ""
 
+    # Creation of the obj file
     for tri in triangles:
         for vertex in tri:
             if vertex not in vertices:
+
                 color = [0,0.5,0]
                 for point in pointCloud:
-                    if vertex[0]==point[0] and vertex[0]==point[0]:
+                    if vertex[0]==point[0] and vertex[1]==point[1]:
                         color = colors[pointToColor(point)]
-                        break        
+                        break    
+
+                # Adding the top points
                 obj += "v " + "{:.8f}".format(vertex[0]) + " " + "{:.8f}".format(vertex[1]) + " " + "{:.8f}".format(maxZ) + " "
                 obj += "{:.8f}".format(color[0]) + " " + "{:.8f}".format(color[1]) + " " + "{:.8f}".format(color[2]) + "\n"
                 vertices[vertex] = index
@@ -118,15 +201,21 @@ def createExtruded2DAlphaShape(pointCloud, colors, alpha, path):
 
 
     for coord in vertices.keys():
+
         color = [0,0.5,0]
         for point in pointCloud:
-                    if coord[0]==point[0] and coord[0]==point[0]:
+                    if coord[0]==point[0] and coord[1]==point[1]:
                         color = colors[pointToColor(point)]
                         break
+
+        # Adding the bottom points 
+        # Note : it was done this way to have the bottom point at the top point index + the number of point in total at the top
+        # e.g. The top point is stored at the index 2 and there are 10 points, the bottom corresponding point is stored at the index 12
         obj += "v " + "{:.8f}".format(coord[0]) + " " + "{:.8f}".format(coord[1]) + " " + "{:.8f}".format(minZ) + " "
         obj += "{:.8f}".format(color[0]) + " " + "{:.8f}".format(color[1]) + " " + "{:.8f}".format(color[2]) + "\n"
 
-        
+    
+    # Adding the top flat part
     for tri in triangles:
         obj += "f"
         for vertex in tri:
@@ -136,7 +225,7 @@ def createExtruded2DAlphaShape(pointCloud, colors, alpha, path):
 
     length = len(vertices.keys())
 
-
+    # Adding the side triangles
     for i in vertices.values():
         if i == length:
             obj += "f " + str(i) + " " + str(i + length) + " " + str(length+1) + "\n"
@@ -155,7 +244,22 @@ def createExtruded2DAlphaShape(pointCloud, colors, alpha, path):
 
 
 def createAlphashape(pointCloud, alpha, colors, path):
+    """Create an alpha shape mesh based on a provided point cloud and alpha parameter and save it as an obj file 
     
+    Parameters
+    ------
+    pointCloud : 2d-array
+        In column, index of the point.  
+        In line, array that contains the x, y and z coordinates of the point.
+    colors : 2d-array
+        In column, index of the point.  
+        In line, array that contains the r, g and b values of the point (between 0 and 255).
+    alpha : float
+        Value dictating the level of detail of the result of the alpha shape algorithm 
+    path : str
+        Path to save the obj file
+
+    """
     start = time.time()
     print("Starting creation of alpha shape")
     alphashapeTree = alphashape(pointCloud, alpha)
@@ -194,6 +298,14 @@ def createAlphashape(pointCloud, alpha, colors, path):
 
 
 def repairAlphaShapeNormals(mesh: trimesh.Trimesh):
+    """Make all normals face away from the center of the bounding box of the mesh 
+    
+    Parameters
+    ------
+    mesh : Trimesh
+        The mesh with normals to work with (using face normals) 
+
+    """
     boundingCenter = (mesh.bounds[0]+mesh.bounds[1])/2
 
     for index in range(mesh.faces.shape[0]):
@@ -202,12 +314,29 @@ def repairAlphaShapeNormals(mesh: trimesh.Trimesh):
         normal = mesh.face_normals[index]
         triangleCenter = (face[0] + face[1] + face[2])/3
         if needToReverseFace(boundingCenter, triangleCenter, normal):
+            # Flipping the normal
             mesh.faces[index] = np.ascontiguousarray(np.flipud(mesh.faces[index]))
 
     return mesh
 
 
 def needToReverseFace(boundingCenter, triangleCenter, normal):
+    """Given the center of the bounding box, the center of the triangle and the normal, returns whether the normal needs to be flipped
+    
+    Parameters
+    ------
+    boundingCenter : array
+        The coordinates (x, y, z) of the bounding box
+    triangleCenter : array
+        The coordinates (x, y, z) of the triangle center
+    normal : array
+        The values (x, y, z) of the normal
+    
+    Returns
+    ------
+    needToBeReversed : bool
+        True if the normal is pointing towards the center of the bounding box
+    """
     direction = triangleCenter - boundingCenter
     direction /= scipy.linalg.norm(direction)
 
