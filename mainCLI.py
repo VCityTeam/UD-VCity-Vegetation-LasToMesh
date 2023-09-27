49c065585b546647b456a3f6c65e3e32dd75d0f5
JulienPercheron <julien_p@icloud.com>
2023-09-27 14:48:35 +0200

    Removed the default value of input parameter

--- mainCLI.py
+++ mainCLI.py
@@ -6,7 +6,7 @@ def main():
 
     parser = argparse.ArgumentParser(description="Transform the vegetation inside a classified point cloud into a set of meshes")
 
-    parser.add_argument("-i", "--input", help="Input las file (default ='./SampleDatas/ExampleDataIsolatedTrees.las')", default="./SampleDatas/ExampleDataIsolatedTrees.las")
+    parser.add_argument("-i", "--input", help="Input las/laz file (mandatory)")
     parser.add_argument("-o", "--output", help="Output directory (default ='./output/')", default="./output/")
     parser.add_argument("-c", "--cellsize", help="Cell size (default = 2.0)", default=2.0, type=float)
     parser.add_argument("-v", "--verbose", help="Increase output verbosity", action="store_true")
